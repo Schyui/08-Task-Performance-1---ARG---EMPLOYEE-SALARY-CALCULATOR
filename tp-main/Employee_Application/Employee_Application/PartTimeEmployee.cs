3f5087c29429f183dd84958201b87b0da5cd9f8f
Idnay, H <131360775+Schyui@users.noreply.github.com>
2024-10-15 20:42:26 +0800

    Add files via upload

---
+++ tp-main/Employee_Application/Employee_Application/PartTimeEmployee.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeNamespace
+{
+
+    class PartTimeEmployee : IEmployee
+    {
+        private string first_name;
+        private string last_name;
+        private string department;
+        private string job_title;
+        private double basic_salary;
+        public PartTimeEmployee(string FName, string LName, string dept, string job)
+        {
+            this.first_name = FName;
+            this.last_name = LName;
+            this.department = dept;
+            this.job_title = job;
+        }
+
+        public string FirstName{
+            get{return this.first_name; }
+            set{ this.first_name = value;}
+        }
+        public string LastName
+        {
+            get { return this.last_name; }
+            set { this.last_name = value; }
+        }
+        public string Department
+        {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+        public string JobTitle
+        {
+            get { return this.job_title; }
+            set { this.job_title = value; }
+        }
+        public double BasicSalary
+        {
+            get { return this.basic_salary; }
+            set { this.basic_salary = value; }
+        }
+
+        public void computeSalary(int hoursWorked, double ratePerHour)
+        {
+            this.basic_salary = hoursWorked * ratePerHour;
+        }
+        public double getSalary()
+        {
+            return this.basic_salary;
+        }
+    }
+}
